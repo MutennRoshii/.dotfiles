74fcae9e83b03a0480cbd2eea6440d55594072a1
MutennRoshii <abdelkarimlamrini@outlook.com>
2024-01-08 00:04:32 +0100

    Improved dunst notification readability

--- bin/volume_brightness.sh
+++ bin/volume_brightness.sh
@@ -27,13 +27,13 @@ function get_volume_icon {
     volume=$(get_volume)
     mute=$(get_mute)
     if [ "$mute" == "yes" ]; then
-        volume_icon="󰝟"
+        volume_icon="󰖁"
     elif [ "$volume" -eq 0 ]; then
-        volume_icon="󰕿"
+        volume_icon=""
     elif [ "$volume" -lt 50 ]; then
-        volume_icon="󰖀"
+        volume_icon=""
     else
-        volume_icon="󰕾"
+        volume_icon=""
     fi
 }
 
