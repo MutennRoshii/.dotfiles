315b1bb1b11574601aa330e8f31f086838aac59e
MutennRoshii <abdelkarimlamrini@outlook.com>
2024-01-07 15:22:40 +0100

    Battery icon & Rofi theme

--- bin/volume_brightness.sh
+++ bin/volume_brightness.sh
@@ -106,13 +106,13 @@ case $1 in
 
     brightness_up)
     # Increases brightness and displays the notification
-    brightnessctl set +2% -n 1
+    brightnessctl set +2% -n 1 -q
     brightness_notification
     ;;
 
     brightness_down)
     # Decreases brightness and displays the notification
-    brightnessctl set 2%- -n 1
+    brightnessctl set 2%- -n 1 -q
     brightness_notification
     ;;
 esac
